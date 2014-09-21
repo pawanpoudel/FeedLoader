e9130af978fb24eeaa75fbf4c8a76f4cef6df1d2
Pawan Poudel <pawan.poudel@gmail.com>
2014-09-21 17:23:14 -0400

    Read feeds from a local JSON file

---
+++ FeedLoader/Feeds/Controllers/FileFeedFetcher.h
@@ -0,0 +1,14 @@
+//
+//  FileFeedFetcher.h
+//  FeedLoader
+//
+//  Created by Pawan Poudel on 9/21/14.
+//  Copyright (c) 2014 Pawan Poudel. All rights reserved.
+//
+
+#import "FeedFetcher.h"
+@import Foundation;
+
+@interface FileFeedFetcher : NSObject <FeedFetcher>
+
+@end
