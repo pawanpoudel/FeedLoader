065d6b89bccf2756edfee1e736e1eeb8c932974e
Pawan Poudel <pawan.poudel@gmail.com>
2014-09-21 17:26:30 -0400

    Create a manager that acts as a façade to feed service

---
+++ FeedLoader/Feeds/Controllers/FeedManager.h
@@ -0,0 +1,47 @@
+//
+//  FeedManager.h
+//  FeedLoader
+//
+//  Created by Pawan Poudel on 9/21/14.
+//  Copyright (c) 2014 Pawan Poudel. All rights reserved.
+//
+
+
+#import "FeedManagerDelegate.h"
+#import "FeedFetcher.h"
+
+@import Foundation;
+@class FeedBuilder;
+
+/**
+    @description A façade providing access to the Feed service.
+                 Application code should only use this class to
+                 get at feed service innards.
+ */
+@interface FeedManager : NSObject
+
+/**
+    @description The delegate object to receive feed retrieval events.
+    @see FeedManagerDelegate
+ */
+@property (nonatomic, weak) id <FeedManagerDelegate> delegate;
+
+/**
+    @description An object that knows how to fetch feed.
+    @param feedFetcher An object that conforms to FeedFetcher protocol.
+ */
+- (void)setFeedFetcher:(id<FeedFetcher>)feedFetcher;
+
+/**
+    @description Sets the feed builder object responsible for creating
+                 Feed objects from JSON retrieved from feed service.
+    @param feedBuilder A DHCFeedBuilder object.
+ */
+- (void)setFeedBuilder:(FeedBuilder *)feedBuilder;
+
+/**
+    @description Initiates retrieval of feeds
+ */
+- (void)fetchFeeds;
+
+@end
