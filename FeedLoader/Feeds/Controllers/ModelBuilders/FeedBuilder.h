4a7750f727174841cc9f7ea367ae26396e4b91e9
Pawan Poudel <pawan.poudel@gmail.com>
2014-09-21 23:05:20 -0400

    Use consistent style for header docs
    Although /*! is perfectly fine for starting a header doc,
    we've been using /** through out the project. So, it makes
    sense to stick with the latter for consistency.

--- FeedLoader/Feeds/Controllers/ModelBuilders/FeedBuilder.h
+++ FeedLoader/Feeds/Controllers/ModelBuilders/FeedBuilder.h
@@ -9,21 +9,21 @@
 @import Foundation;
 @class FeedDataManager;
 
-/*!
+/**
     @description Construct Feed objects from JSON array.
     @note The format of the JSON is driven by the feed service.
     @see Feed
  */
 @interface FeedBuilder : NSObject
 
-/*!
+/**
     @description Sets the feed data manager that knows how to create,
                  update, delete and retrieve Feed objects.
     @param feedDataManager A FeedDataManager object.
  */
 - (void)setFeedDataManager:(FeedDataManager *)feedDataManager;
 
-/*!
+/**
     @description Given an array containing JSON dictionaries, returns a list
                  of Feed objects.
     @param JSON The deserialized JSON object in NSArray format.
