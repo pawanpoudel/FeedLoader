b3ee480e62f55c4fc4c608943199ca8d5ab54c4f
Pawan Poudel <pawan.poudel@gmail.com>
2014-09-21 22:54:34 -0400

    Feed model is now shareable

--- FeedLoader/Feeds/Models/Feed.m
+++ FeedLoader/Feeds/Models/Feed.m
@@ -18,4 +18,12 @@
 @dynamic summary;
 @dynamic title;
 
+#pragma mark - MDShareable protocol methods
+
+- (NSString *)textToShare {
+    NSDictionary *appInfo = [[NSBundle mainBundle] infoDictionary];
+    NSString *bundleDisplayName = appInfo[@"CFBundleDisplayName"];
+    return [NSString stringWithFormat:@"%@\n%@\n\nShared via %@", self.title, self.sourceUrl, bundleDisplayName];
+}
+
 @end
